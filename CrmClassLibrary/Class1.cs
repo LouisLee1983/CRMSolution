065130f5d1f367a21a7ea9e89dc672d3033bead5
Administrator <Administrator@LJYPC-HOME>
2017-02-03 13:40:28 +0800

    member

---
+++ CrmClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmClassLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ CrmClassLibrary/Member/AppName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmClassLibrary.Member
+{
+    public class AppName
+    {
+        public string Name { get; set; }
+
+        public string Desp { get; set; }
+
+    }
+}
