4a355f7202060154ff7cede38caf909e5d1d2904
Administrator <Administrator@LJYPC-HOME>
2017-03-19 15:47:01 +0800

    aa

--- CrmWebApp/Controllers/AdminController.cs
+++ CrmWebApp/Controllers/AdminController.cs
@@ -1,26 +1,30 @@
-﻿using System;
+﻿using CrmWebApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
-using CrmWebApp.Models;
 
 namespace CrmWebApp.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
-        [Authorize(Roles ="Admin")]
+        [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
+        [Authorize(Roles = "Admin")]
         public ActionResult UpdateOtaCompany()
         {
             OtaCrmModel db = new OtaCrmModel();
-            
+
             var companycmss = from p in db.CompanyCmsData
                               select p;
             Dictionary<string, CompanyCmsData> cmsDict = new Dictionary<string, CompanyCmsData>();
@@ -31,26 +35,68 @@ namespace CrmWebApp.Controllers
                     cmsDict.Add(cmsItem.CompanyName, cmsItem);
                 }
             }
-            var otacompanys = from p in db.OtaCompany
-                              select p;
+            var q = from o in db.OtaCompany
+                    select o;
+            List<OtaCompany> otacompanys = q.ToList();
 
+            int updateCount = 0;
+            bool isUpdate = false;
             foreach (OtaCompany cItem in otacompanys)
             {
                 if (cmsDict.ContainsKey(cItem.CompanyName))
                 {
                     CompanyCmsData cmsItem = cmsDict[cItem.CompanyName];
                     //需要修改得地方是这些
-                    cItem.SalesUserName = string.IsNullOrEmpty(cItem.SalesUserName)? cmsItem.SalesName:"";
-                    cItem.RegisterAddress = string.IsNullOrEmpty(cItem.RegisterAddress)? cmsItem.RegisterAddress:"";
-                    cItem.RealAddress = string.IsNullOrEmpty(cItem.RealAddress)? cmsItem.RealAddress:"";
-                    cItem.BossBackground = string.IsNullOrEmpty(cItem.BossBackground)? cmsItem.BossBackground:"";
-                    cItem.BossName = string.IsNullOrEmpty(cItem.BossName)? cmsItem.ContactPerson:"";
-                    cItem.BusinessStatus = string.IsNullOrEmpty(cItem.BusinessStatus)? cmsItem.TTSStatusDesp:"";
-                    cItem.LegalPerson = string.IsNullOrEmpty(cItem.LegalPerson)? cmsItem.LegalPerson:"";
-                    cItem.LegalPersonPhone = string.IsNullOrEmpty(cItem.LegalPersonPhone)? cmsItem.ContactPhone:"";
-                    
+                    if (!string.IsNullOrEmpty(cmsItem.SalesName))
+                    {
+                        cItem.SalesUserName = cmsItem.SalesName;
+                    }
+                    if (string.IsNullOrEmpty(cItem.RegisterAddress) && !string.IsNullOrEmpty(cmsItem.RegisterAddress))
+                    {
+                        isUpdate = true;
+                        cItem.RegisterAddress = cmsItem.RegisterAddress;
+                    }
+                    if (string.IsNullOrEmpty(cItem.RealAddress) && !string.IsNullOrEmpty(cmsItem.RealAddress))
+                    {
+                        isUpdate = true;
+                        cItem.RealAddress = cmsItem.RealAddress;
+                    }
+                    if (string.IsNullOrEmpty(cItem.BossBackground) && !string.IsNullOrEmpty(cmsItem.BossBackground))
+                    {
+                        isUpdate = true;
+                        cItem.BossBackground = cmsItem.BossBackground;
+                    }
+                    if (string.IsNullOrEmpty(cItem.BossName) && !string.IsNullOrEmpty(cmsItem.ContactPerson))
+                    {
+                        isUpdate = true;
+                        cItem.BossName = cmsItem.ContactPerson;
+                    }
+                    if (string.IsNullOrEmpty(cItem.BusinessStatus) && !string.IsNullOrEmpty(cmsItem.TTSStatusDesp))
+                    {
+                        isUpdate = true;
+                        cItem.BusinessStatus = cmsItem.TTSStatusDesp;
+                    }
+                    if (string.IsNullOrEmpty(cItem.LegalPerson) && !string.IsNullOrEmpty(cmsItem.LegalPerson))
+                    {
+                        isUpdate = true;
+                        cItem.LegalPerson = cmsItem.LegalPerson;
+                    }
+                    if (string.IsNullOrEmpty(cItem.LegalPersonPhone) && !string.IsNullOrEmpty(cmsItem.ContactPhone))
+                    {
+                        isUpdate = true;
+                        cItem.LegalPersonPhone = cmsItem.ContactPhone;
+                    }
+                    if (isUpdate)
+                    {
+                        updateCount++;
+                    }
                 }
+                isUpdate = false;
             }
+            db.SaveChangesAsync();
+
+            string result = "成功更新:" + updateCount.ToString();
+            return Content(result);
         }
     }
 }
